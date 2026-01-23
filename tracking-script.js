4d9a9344bfb91ac3e51e4b822289e5a4c3f3e22c
Hacker114 <orrysander@gmail.com>
2026-01-23 14:17:20 -0800

    Create tracking-script.js

---
+++ tracking-script.js
@@ -0,0 +1,2 @@
+window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
+cronitor('config', { clientKey: 'b72461a152926ec0d8e6d87824007208' });
