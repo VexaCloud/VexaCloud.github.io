360e7165985d7a9aca54d2b9e17ed5e5950e782f
VexaCloud <256695911+VexaCloud@users.noreply.github.com>
2026-08-25 18:19:11 -0700

    Change target selectors for message interactions

--- script.js
+++ script.js
@@ -3429,7 +3429,7 @@ notificationSettingsSave.onclick = async () => {
         icon: '<path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5Z"/>',
         title: "Messages",
         desc: "The main area shows the current channel’s messages. Newer messages appear at the bottom. You can upload and paste files as well.",
-        target: "#chat-body",
+        target: "#message-list",
         placement: "left",
       },
       {
@@ -3443,7 +3443,7 @@ notificationSettingsSave.onclick = async () => {
         icon: '<path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/><path d="M3 3v5h5"/>',
         title: "Reply, react & mention",
         desc: "Right‑click a message to reply or react with an emoji. Type @ to mention someone else in the server.",
-        target: "#.chat-input-area",
+        target: "#message-list",
         placement: "top",
       },
       {
